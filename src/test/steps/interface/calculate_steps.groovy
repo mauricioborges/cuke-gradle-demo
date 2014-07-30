1fc44991354a3bdea4547ab6db89a19e954a58b0
Maurício Borges <mauricioborges@gmail.com>
2014-07-29 23:56:09 -0300

    added multiple tasks that are defined according to the different possibilities of step definition. Now you can define different sources for steps, and different cucumber tasks are created according to it

---
+++ src/test/steps/interface/calculate_steps.groovy
@@ -0,0 +1,10 @@
+import static cucumber.api.groovy.EN.*
+
+import cucumber.api.PendingException
+
+Given(~/^the input "(.*?)"$/) { String arg1 ->
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException()
+}
+
+
